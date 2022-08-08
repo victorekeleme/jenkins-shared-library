602520421411828dd5d799061223fc0654014430
victorekeleme <ekelemevictor99@gmail.com>
2022-08-08 18:35:11 -0400

    initial commit

---
+++ vars/buildimage.groovy
@@ -0,0 +1,9 @@
+#!usr/bin/env groovy
+def call() {
+    echo "Building the application docker image"
+    withCredentials([usernamePassword(credentialsId: 'Docker-Repo-Credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh "docker build -t vistein12/java-maven-app:${VERSION} ."
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "docker push vistein12/java-maven-app:${VERSION}"
+    }
+}
