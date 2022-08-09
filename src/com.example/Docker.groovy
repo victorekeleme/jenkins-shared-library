886eceaafc631f30306ba0d173f459aae3ca8afa
victorekeleme <ekelemevictor99@gmail.com>
2022-08-08 21:43:00 -0400

    package docker

---
+++ src/com.example/Docker.groovy
@@ -0,0 +1,20 @@
+#!usr/bin/env groovy
+package com.example
+
+class Docker implements Serialiazable {
+
+    def script
+
+    def Docker(script) {
+        this.script = script
+    }
+
+    def buildDockerImage(String imageName) {
+        script.echo "Building the application docker image"
+        script.withCredentials([script.usernamePassword(credentialsId: 'Docker-Repo-Credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        script.sh "docker build -t $imageName ."
+        script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
+        script.sh "docker push $imageName"
+    }
+
+}
--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,6 @@
 #!usr/bin/env groovy
+import com.example.Docker
 
 def call() {
-    echo "Building the application docker image"
-    withCredentials([usernamePassword(credentialsId: 'Docker-Repo-Credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t vistein12/java-maven-app:1.2 ."
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push vistein12/java-maven-app:1.2"
-    }
+    return new Docker(this).buildDockerImage(imageName)
 }
