e51dc807dfb45dab3cddf90f130311fb9776106c
victorekeleme <ekelemevictor99@gmail.com>
2022-08-18 00:09:49 -0400

    removed files

--- vars/buildJar.groovy
+++
@@ -1,6 +0,0 @@
-#!usr/bin/env groovy
-
-def call(){
-    echo "Building application jar"
-    sh "mvn clean package"
-}
--- vars/dockerLogin.groovy
+++
@@ -1,6 +0,0 @@
-#!usr/bin/env groovy
-import com.example.Docker
-
-def call() {
-    return new Docker(this).login()
-}
