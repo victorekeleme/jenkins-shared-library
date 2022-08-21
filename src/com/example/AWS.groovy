970c1468cac68ead990fa38fbcf353f56a580340
victorekeleme <ekelemevictor99@gmail.com>
2022-08-21 07:22:24 -0400

    added script

--- src/com/example/AWS.groovy
+++ src/com/example/AWS.groovy
@@ -9,17 +9,17 @@ class AWS implements Serializable {
         this.script = script
     }
     def loginAWSECR() {
-        withCredentials([aws(accessKeyVariable:'AWS_ACCESS_KEY_ID',credentialsId:'aws-credentials',secretKeyVariable:'AWS_SECRET_ACCESS_KEY')]) {
-        sh "aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 524360703326.dkr.ecr.us-east-2.amazonaws.com"
+        script.withCredentials([aws(accessKeyVariable:'AWS_ACCESS_KEY_ID',credentialsId:'aws-credentials',secretKeyVariable:'AWS_SECRET_ACCESS_KEY')]) {
+        script.sh "aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 524360703326.dkr.ecr.us-east-2.amazonaws.com"
     }
     }
     def buildAWSDockerImage(String imageName) {
-       sh "docker build -t $imageName ."
+       script.sh "docker build -t $imageName ."
 
     }
     def pushDockerImageAWS(String imageName) {
-        sh "docker tag $imageName 524360703326.dkr.ecr.us-east-2.amazonaws.com/$imageName"
-        sh "docker push 524360703326.dkr.ecr.us-east-2.amazonaws.com/$imageName"
+        script.sh "docker tag $imageName 524360703326.dkr.ecr.us-east-2.amazonaws.com/$imageName"
+        script.sh "docker push 524360703326.dkr.ecr.us-east-2.amazonaws.com/$imageName"
     }
    
 }
