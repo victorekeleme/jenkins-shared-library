05e958899f687f008b10fb46b2b26db7d117a8df
victorekeleme <ekelemevictor99@gmail.com>
2022-08-21 17:54:29 -0400

    fixes

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -10,26 +10,19 @@ class Docker implements Serializable {
     }
 
     def buildDockerImage(String imageName) {
-        script.withCredentials([script.usernamePassword(credentialsId: 'docker-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
-
         script.echo "Building the application docker image"
         script.sh "docker build -t $imageName ."
-
-        script.sh "docker push $imageName"
-
         }
 
+
+    def login() {
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
+        }
     }
 
-    // def login() {
-    //     script.withCredentials([script.usernamePassword(credentialsId: 'docker-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-    //         script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
-    //     }
-    // }
-
-    // def pushDockerImage(String imageName) {
-    //     script.sh "docker push $imageName"
-    // }        
+    def pushDockerImage(String imageName) {
+        script.sh "docker push $imageName"
+    }        
 
 }
