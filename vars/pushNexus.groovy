b90dd85b4cd4fad08db1a91b319d4fa465c24d59
victorekeleme <ekelemevictor99@gmail.com>
2022-08-21 10:20:35 -0400

    javahome

--- vars/pushNexus.groovy
+++ vars/pushNexus.groovy
@@ -2,5 +2,8 @@
 import com.example.Nexus
 
 def call(String imageName) {
-    return new Nexus(this).nexusLoginPush(imageName)
+    sh "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64"
+    sh "echo $JAVA_HOME"
+    sh "groovy --version"
+    // return new Nexus(this).nexusLoginPush(imageName)
 }
